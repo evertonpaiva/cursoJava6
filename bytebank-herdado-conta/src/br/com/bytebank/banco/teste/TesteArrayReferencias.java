a2cab928493f76c4a833204c8d9ada07283626e9
Everton Paiva <evertonpaiva@gmail.com>
2019-07-09 11:59:11 -0300

    Entendendo o array String args

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
@@ -1,5 +1,6 @@
 package br.com.bytebank.banco.teste;
 
+import br.com.bytebank.banco.modelo.Cliente;
 import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.ContaPoupanca;
@@ -8,18 +9,21 @@ public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
 		
-		Conta[] contas = new Conta[5];
+		Object[] referencias = new Object[5];
 		
 		ContaCorrente cc1 = new ContaCorrente(11, 1111);
-		contas[0] = cc1;
+		referencias[0] = cc1;
 		
 		ContaPoupanca cc2 = new ContaPoupanca(22, 2222);        
-        contas[1] = cc2;
+        referencias[1] = cc2;
         
-        System.out.println(contas[0].toString());
+        Cliente cliente = new Cliente();
+        referencias[2] = cliente;
         
-        ContaPoupanca ref = (ContaPoupanca) contas[1];
-        //ContaCorrente ref2 = (ContaCorrente) contas[1];
+        //System.out.println(contas[1].getNumero());
+        
+        ContaPoupanca ref = (ContaPoupanca) referencias[1];
+        //ContaCorrente ref2 = (ContaCorrente) referencias[1];
         
         System.out.println(cc2.toString());
         System.out.println(ref.toString());
